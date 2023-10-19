b39fb91ff63327289e8fa5f11372025ae83f9f3f
20-42905-1 <aponriaze12022000@gmail.com>
2023-10-19 23:45:36 +0600

    My first

---
+++ LabTaskk/Controllers/CategoryController.cs
@@ -0,0 +1,32 @@
+﻿using LabTaskk.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LabTaskk.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // GET: Category
+        public ActionResult Category()
+        {
+            var db = new LabTaskEntities2();
+            var data = db.Categories1.ToList();
+            return View(data);
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        public ActionResult Edit()
+        {
+            return View();
+        }
+        public ActionResult Delete()
+        {
+            return View();
+        }
+    }
+}
---
+++ LabTaskk/Controllers/ProductController.cs
@@ -0,0 +1,36 @@
+﻿using LabTaskk.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LabTaskk.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            var db = new LabTaskEntities2();
+            var data = db.Products.ToList();
+            return View(data);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+        public ActionResult Edit()
+        {
+
+            return View();
+        }
+        public ActionResult Delete()
+        {
+
+            return View();
+        }
+
+    }
+}
